83f24072748ab5714d178eca7d34b7b2f6ccb8c3
Juan Bautista Quiroga <juanbaquiroga@gmail.com>
2022-08-25 16:15:26 -0300

    first commit

--- amate/settings.py
+++ amate/settings.py
@@ -138,11 +138,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR/'static',
-]
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [BASE_DIR/'static',]
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
