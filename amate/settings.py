2dfcebb34b776a7b6b40a08fff9ebe14a3c52715
Juan Bautista Quiroga <juanbaquiroga@gmail.com>
2022-08-24 22:46:50 -0300

    first commit

--- amate/settings.py
+++ amate/settings.py
@@ -84,13 +84,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'amate.wsgi.application'
 
 
+
 # Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'juanbaquiroga$amate',
+        'USER': 'juanbaquiroga',
+        'PASSWORD': 'pythonsql',
+        'HOST': 'juanbaquiroga.mysql.pythonanywhere-services.com',
     }
 }
 
