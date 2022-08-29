fbe7ff0baee4ac5d2b7efe6e193997221a54eed5
Juan Bautista Quiroga <juanbaquiroga@gmail.com>
2022-08-28 22:54:34 -0300

    agregado de media

--- amate/urls.py
+++ amate/urls.py
@@ -25,3 +25,4 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('products/', include('Productos.urls')),
 ]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
++ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
